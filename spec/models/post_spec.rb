2d3eb7c7f729e6ffa5a706873a88b4c81c51f6e6
Liam Ellis <liamhellis@gmail.com>
2016-10-02 13:56:41 -0700

    factory girl test passed for post with user_id

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,5 +3,17 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   it { should validate_presence_of :title }
   it { should validate_presence_of :content }
-  
+
+  post = FactoryGirl.create(:post, user_id: 1)
+  it 'has a default title' do
+    expect(post.title).to eq "Check this out guys"
+  end
+
+  it 'has default content' do
+    expect(post.content).to eq "stuff to check out"
+  end
+
+  it 'has default url' do
+    expect(post.url).to eq "www.google.com"
+  end
 end
